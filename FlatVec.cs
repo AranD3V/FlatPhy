b9b8c391f4914745bdf090b791d6c05f1ee50a7e
aditARAN <154549873+AranD3V@users.noreply.github.com>
2024-10-04 23:24:05 +0530

    Add project files.

---
+++ FlatVec.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatPhy
+{
+    public readonly struct FlatVec
+    {
+        public readonly float X; 
+        public readonly float Y;
+
+        public static readonly FlatVec Zero = new FlatVec(0f, 0f);
+
+        public FlatVec(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public static FlatVec operator +(FlatVec a, FlatVec b) 
+        {
+            return new FlatVec(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static FlatVec operator -(FlatVec a, FlatVec b)
+        {
+            return new FlatVec(a.X - b.X, a.Y - b.Y);
+        }
+
+        public static FlatVec operator -(FlatVec v)
+        {
+            return new FlatVec(-v.X, -v.Y);
+        }
+
+        public static FlatVec operator *(FlatVec v, float s)
+        {
+            return new FlatVec(v.X * s, v.Y * s);
+        }
+
+        public static FlatVec operator /(FlatVec v, float s)
+        {
+            return new FlatVec(v.X / s, v.Y / s);
+        }
+
+        public bool Equal(FlatVec other)
+        {
+            return this.X == other.X && this.Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj is FlatVec other)
+            {
+                return this.Equal(other);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return new { this .X, this.Y }.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"X: {this.X}, Y:{this.Y}";
+        }
+
+    }
+}
